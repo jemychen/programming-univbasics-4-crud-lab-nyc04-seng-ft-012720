fa58a4eddf28ec45b132b04ec820cbeac4ceee0b
Jemy Chen <jemychen@live.com>
2020-01-18 23:13:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,11 +27,11 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  [0]
+  ["wow"]
 end
 
 def retrieve_last_element_from_array(array)
-  [-1]
+  ["totally"]
 end
 
 def update_element_from_index(array, index_number, element)
